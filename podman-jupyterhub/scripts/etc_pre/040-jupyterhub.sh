43bedb1e1c2fbcb6330cf2dd61b2ecbd477255a5
Ben Du <longendu@gmail.com>
2026-05-16 00:07:07 -0700

    update scripts

--- podman-jupyterhub/scripts/etc_pre/040-jupyterhub.sh
+++ podman-jupyterhub/scripts/etc_pre/040-jupyterhub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # add admin user to JupyterHub
-sed -i "s/DOCKER_ADMIN_USER/${DOCKER_ADMIN_USER:-$(id -un)}/g" /etc/jupyterhub/jupyterhub_config.py
+sed -i "s/JUPYTERHUB_ADMIN_USER/${JUPYTERHUB_ADMIN_USER:-$(id -un)}/g" /etc/jupyterhub/jupyterhub_config.py
 # user memory limit
 sed -i "s/USER_MEM_LIMIT/${USER_MEM_LIMIT:-4G}/g" /etc/jupyterhub/jupyterhub_config.py

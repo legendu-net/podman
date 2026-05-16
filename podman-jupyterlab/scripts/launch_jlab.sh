6ad5b2c1746219842871bcda3c1b8b0451fa54fd
Ben Du <longendu@gmail.com>
2026-05-15 20:28:28 -0700

    refactor /scripts/sys/*.sh to be /scripts/*.sh

--- podman-jupyterlab/scripts/launch_jlab.sh
+++ podman-jupyterlab/scripts/launch_jlab.sh
@@ -2,8 +2,8 @@
 # This script is for launching a JupyterLab service 
 # in situations where launch.sh in dclong/jupyterlab is overwritten (e.g., in dclong/vscode-server).
 # There are 2 popular use cases.
-# 1. Pass the CMD "/scripts/sys/init.sh /scripts/sys/launch_jlab.sh" to the docker command when launching a Docker container.
-# 2. Run "/scripts/sys/launch_jlab.sh" in an already launched Docker container.
+# 1. Pass the CMD "/scripts/init.sh /scripts/launch_jlab.sh" to the docker command when launching a Docker container.
+# 2. Run "/scripts/launch_jlab.sh" in an already launched Docker container.
 
 #@TODO: add --collaborative when it's stable
 

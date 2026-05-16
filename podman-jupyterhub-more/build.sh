ca49c6c77943bd840c360828ade6917351e740ce
Ben Du <longendu@gmail.com>
2026-05-16 00:24:57 -0700

    update scripts

--- podman-jupyterhub-more/build.sh
+++ podman-jupyterhub-more/build.sh
@@ -2,5 +2,5 @@
 
 podman pull quay.io/legendu/jupyterhub-jdk:next
 podman build -t quay.io/legendu/jupyterhub-more:next .
-podman push quay.io/legendu/jupyterhub-more:next
 #podman build --no-cache -t quay.io/legendu/jupyterhub-more:next .
+podman push quay.io/legendu/jupyterhub-more:next
--- podman-jupyterhub-more/scripts/etc_pre/060-jupyterhub-more.sh
+++ podman-jupyterhub-more/scripts/etc_pre/060-jupyterhub-more.sh
@@ -2,6 +2,3 @@
 
 icon zellij -c
 
-if [ -e "$HOME/.gemini" ]; then
-    ln -snf $HOME/.gemini ~/.gemini
-fi

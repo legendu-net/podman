23e96a2fbd4e3e2be8a682a32663df6e9f400286
Ben Du <longendu@gmail.com>
2026-05-16 17:15:25 -0700

    refactor and simplify base/scripts/init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,37 +1,17 @@
 #!/usr/bin/env bash
 
-function scripts_etc.usage() {
-    cat << EOF
-NAME
-    scripts_etc - configuration for init
-
-SYNTAX 
-    scripts_etc pre/post [upper_index]
-
-ARGUMENT
-    pre/post: Run scripts in /scripts/etc_pre or /scripts/etc_post respectively.
-    upper_index: The maximum index of scripts to run.
-
-DESCRIPTION
-    Run scripts in /scripts/etc_pre or /scripts/etc_post 
-    which are for automatic configurations in Docker container.
-
-EOF
-}
-
-function scripts_etc() {
-    local upper_index=9${2:-999}
+# _config pre/post
+# Run scripts matching /scripts/pre-[0-9][0-9][0-9]-*.sh or
+# /scripts/post-[0-9][0-9][0-9]-*.sh for automatic container configuration.
+function _config() {
     local script
-    for script in $(ls /scripts/etc_$1/[0-9][0-9][0-9]-*.sh 2> /dev/null); do
-        local index=$(basename $script)
-        index=9${index:0:3}
-        if [[ $index -le $upper_index ]]; then
-            echo "Sourcing in the script $script ..."
-            source $script
-        fi
+    for script in $(ls /scripts/$1-[0-9][0-9][0-9]-*.sh 2> /dev/null); do
+        echo "Sourcing in the script $script ..."
+        source $script
     done
 }
 
-scripts_etc pre
-/scripts/launch.sh
+_config pre
+/scripts/launch.sh &
+_config post
 

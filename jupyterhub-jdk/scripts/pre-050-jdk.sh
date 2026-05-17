c42cf0eda3f6c992b891a95a112566788526fc06
Ben Du <longendu@gmail.com>
2026-05-17 01:17:18 -0700

    fix issue: install jdk 25

--- jupyterhub-jdk/scripts/pre-050-jdk.sh
+++ jupyterhub-jdk/scripts/pre-050-jdk.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export M2_HOME=/usr/share/maven
-export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-25-openjdk
 export PATH=$PATH:$JAVA_HOME/bin
